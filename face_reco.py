8bb16c6d2330d4e05135f60520e4cf3cd6f31e8d
重田 将人 <masato.0928.j@gmail.com>
2020-01-16 18:55:42 +0900

    lambdaで動いているものをコミット

--- face_reco.py
+++ face_reco.py
@@ -3,6 +3,7 @@
 import boto3
 
 import aws_settings as aws
+from invalid_img_exception import Invalid_img_exception
 
 def rekoginition_face(img):
     """
@@ -16,49 +17,82 @@ def rekoginition_face(img):
     Returns
     -------
     analysis_result : dict
-        分析結果。要素には感情、年齢、性別が入っている。
+        分析結果
     """
     try:
-        bucket= aws.BUCKET_NAME
+        print("==  rekoginition_face start==")
+        BUCKET= aws.BUCKET_NAME
         client = boto3.client('rekognition')
+        response = client.detect_faces(Image={'S3Object': {'Bucket': BUCKET, 'Name': img}}, Attributes=['ALL'])
         
-        response = client.detect_faces(Image={'S3Object': {'Bucket': bucket, 'Name': img}}, Attributes=['ALL'])
-
         details = response["FaceDetails"]
+
+        # 分析に失敗した場合。
+        if len(details) == 0:
+            raise Invalid_img_exception
+        
         # 分析結果
         analysis_result = []
         for detail in details:
             emotions = [emotion for emotion in detail["Emotions"]]
             emotion_dict = make_emotion_list(emotions)
             
-            # 感情
+            # emotion
             emotion = emotion_dict[sorted(emotion_dict.keys())[-1]]
-            # 年齢
+            # age_range
             age_range = f'{detail["AgeRange"]["Low"]} - {detail["AgeRange"]["High"]}'
-            # 性別
-            gender = f'{detail["Gender"]["Value"]}'
-            # 年齢幅
+            # gender
+            gender = detail["Gender"]["Value"]
+            # BoundingBox
             bouding_box = detail["BoundingBox"]
-
+            # Smile
+            smile = detail["Smile"]["Value"]
+            # Eyeglasses
+            eyeglasses = detail["Eyeglasses"]["Value"]
+            # Sunglasses
+            sunglasses = detail["Sunglasses"]["Value"]
+            # Beard
+            beard = detail["Beard"]["Value"]
+            # Mustache
+            mustache = detail["Mustache"]["Value"]
+            # EyesOpen
+            eyesopen = detail["EyesOpen"]["Value"]
+            # MouthOpen
+            mouthopen = detail["MouthOpen"]["Value"]
+            
             analysis_result.append({
                 ""
                 "Emotion": emotion,
                 "AgeRange": age_range,
                 "Gender": gender,
-                "BoundingBox" : bouding_box
+                "BoundingBox" : bouding_box,
+                "Smile": smile,
+                "Eyeglasses": eyeglasses,
+                "Sunglasses": sunglasses,
+                "Beard": beard,
+                "Mustache": mustache,
+                "EyesOpen": eyesopen,
+                "MouthOpen": mouthopen
             })
-
+            
+            print("==  rekoginition_face end==")
             return analysis_result
-
-    except Exception as e:
-        print(e)
+    except Exception:
+        raise
 
 def make_emotion_list(emotions):
-    '''
+    """
     emotionリストを作って返す
-    '''
-    emotion_dict = {}
-    for emotion in emotions:
-        emotion_dict[emotion["Confidence"]] = emotion["Type"]
-    return emotion_dict  # array[0] -> Confidence
-                         # array[1] -> Type
+    """
+    try:
+        print("==  make_emotion_list start==")
+        emotion_dict = {}
+        for emotion in emotions:
+            emotion_dict[emotion["Confidence"]] = emotion["Type"]
+            
+        print("==  make_emotion_list end==")
+
+        return emotion_dict  # key -> Confidence
+                             # value -> Type
+    except Exception:
+        raise
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,13 +1,34 @@
 # coding: utf-8
 
-# botアカウントのトークンを指定
-API_TOKEN = "XXXXXXXXXXXX"
+# botアカウントのトークン
+API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXX"
+
+# レガシートークン
+LEGACY_TOKEN = "XXXXXXXXXXXXXXXXXXXXX"
 
 # slackポストURL
-POST_URL = "https://slack.com/api/chat.postMessage"
+MESSAGE_POST_URL = "https://slack.com/api/chat.postMessage"
 
-# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
-PLUGINS = ['plugins']
+# slackAPI
+FILE_LIST_URL = "https://slack.com/api/files.list"
+
+#画像保存パス
+DIR_PATH = "/tmp/"
 
 # bot名
-BOT_NAME = "dev"
+BOT_NAME = "rekobot"
+
+#ICON_URL
+ICON_URL = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+#color
+COLOR = "#EEEEEE"
+
+# ロードtextリスト
+TEXTS = [
+    ":robot_face:   please wait . . .",
+    ":necktie:  please wait . . .",
+    ":jeans:  please wait . . ."
+    ]
+# 解析失敗メッセージ
+DETECT_FACE_FAILURE_MESSAGE = "detect_face_failure (|:>)"
